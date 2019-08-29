4bbf79c2cf4b07f8ef300a6368a63aa79ed533ca
tamekalee <53798890+tamekalee@users.noreply.github.com>
2019-08-29 12:30:38 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
-// Select color input
-// Select size input
+var color = "#000000";
 
-// When size is submitted by the user, call makeGrid()
+document.querySelector("#colorPicker").addEventListener("change", function(evt) {
+color = evt.target.value;
+});
 
-function makeGrid() {
 
-// Your code goes here!
+document.getElementById("sizePicker").addEventListener("submit", function(ev) {
+ev.preventDefault();
+var height = document.querySelector("#inputHeight").value;
+var width = document.querySelector("#inputWidth").value;
 
+var pixelCanvas = document.querySelector("#pixelCanvas");
+pixelCanvas.innerHTML = "";
+var tBody = makeGrid(height, width, color);
+pixelCanvas.appendChild(tBody);
+
+});
+
+function makeGrid(height, width) {
+var tBody = document.createElement("tbody");
+for (var row = 0; row < height; row++) {
+var tr = document.createElement("tr");
+for (var column = 0; column < width; column++) {
+var td = document.createElement("td");
+td.addEventListener("click", function(evt) {
+evt.target.style.background = color;
+})
+tr.appendChild(td);
+}
+tBody.appendChild(tr);
+}
+return tBody;
 }
